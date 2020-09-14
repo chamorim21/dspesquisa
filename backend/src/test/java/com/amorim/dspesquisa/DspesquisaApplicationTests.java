794db7d9d96e13ca81f1726e0b221581cf5449c4
Cristiano Amorim <cristianoretiro2003@gmail.com>
2020-09-14 11:22:13 -0300

    Project created

---
+++ backend/src/test/java/com/amorim/dspesquisa/DspesquisaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amorim.dspesquisa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DspesquisaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
